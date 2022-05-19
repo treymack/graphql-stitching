37818a7e16ff8b3c0bfda5fb16f06ca3be5ec6d6
Trey Mack <tmack10@gmail.com>
2022-05-19 15:35:34 -0400

    very basic stitching (no relationships)

---
+++ HotChocolateV12.Stitching.Gateway.Gql/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient(WellKnownSchemaNames.Musicians,
+    c => c.BaseAddress = new Uri("http://localhost:5101/graphql"));
+builder.Services.AddHttpClient(WellKnownSchemaNames.Bands,
+    c => c.BaseAddress = new Uri("http://localhost:5102/graphql"));
+
+builder.Services
+    .AddGraphQLServer()
+    // .AddQueryType<Query>();
+    .AddRemoteSchema(WellKnownSchemaNames.Musicians)
+    .AddRemoteSchema(WellKnownSchemaNames.Bands)
+    ;
+
+var app = builder.Build();
+
+app.MapGraphQL();
+
+app.Run();
---
+++ HotChocolateV12.Stitching.Gateway.Gql/WellKnownSchemaNames.cs
@@ -0,0 +1,7 @@
+namespace HotChocolateV12.Stitching.Gateway.Gql;
+
+public static class WellKnownSchemaNames
+{
+    public const string Musicians = "musicians";
+    public const string Bands = "bands";
+}
